70785565abf3041bcdf9577cf919bb6f1c0ea07a
kasia.kordys <kasia.kordys@gmail.com>
2020-10-02 14:21:29 +0100

    add custom routing

--- UrlsAndRouting/App_Start/RouteConfig.cs
+++ UrlsAndRouting/App_Start/RouteConfig.cs
@@ -13,11 +13,10 @@ namespace UrlsAndRouting
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("MyRoute", "{controller}/{action}");
+                //name: "Default",
+                //url: "{controller}/{action}/{id}",
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }  
         }
     }
 }
