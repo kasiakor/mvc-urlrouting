048979db849d8071645e183a78b4d3a0d6d919bc
kasia.kordys <kasia.kordys@gmail.com>
2020-10-06 16:47:39 +0100

    add attribute routing constraints

--- UrlsAndRouting/Controllers/CustomerController.cs
+++ UrlsAndRouting/Controllers/CustomerController.cs
@@ -19,10 +19,18 @@ namespace UrlsAndRouting.Controllers
             return View("ActionName");
         }
 
-        [Route("Users/Add/{user}/{id}")]
+        //if we dont add the contraint int
+        //the current request is ambiguous between the following action methods
+        [Route("Users/Add/{user}/{id:int}")]
         public string Create(string user, int id)
         {
             return string.Format("The user name is {0}, and her id is {1}", user, id);
+        }
+
+        [Route("Users/Add/{user}/{password}")]
+        public string CghangePassword(string user, string password)
+        {
+            return string.Format("The user name is {0}, and her new password is {1}", user, password);
         }
     }
 }
