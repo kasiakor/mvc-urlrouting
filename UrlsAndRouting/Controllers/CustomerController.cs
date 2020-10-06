cdefa7572d1ea144b968647e918ac94b43b9da80
kasia.kordys <kasia.kordys@gmail.com>
2020-10-06 17:12:28 +0100

    add route prefix

--- UrlsAndRouting/Controllers/CustomerController.cs
+++ UrlsAndRouting/Controllers/CustomerController.cs
@@ -6,10 +6,12 @@ using System.Web.Mvc;
 
 namespace UrlsAndRouting.Controllers
 {
+    [RoutePrefix("Users")]
     public class CustomerController : Controller
     {
         // GET: Customer
-        [Route("Test")]
+        //~ prefix not applied
+        [Route("~/Test")]
         public ActionResult Index(string id)
         {
             ViewBag.Controller = "Customer";
@@ -21,14 +23,14 @@ namespace UrlsAndRouting.Controllers
 
         //if we dont add the contraint int
         //the current request is ambiguous between the following action methods
-        [Route("Users/Add/{user}/{id:int}")]
+        [Route("Add/{user}/{id:int}")]
         public string Create(string user, int id)
         {
             return string.Format("The user name is {0}, and her id is {1}", user, id);
         }
-
-        [Route("Users/Add/{user}/{password}")]
-        public string CghangePassword(string user, string password)
+        // prefix used  [Route("Users/Add/{user}/{password}")]
+        [Route("Add/{user}/{password}")]
+        public string ChangePassword(string user, string password)
         {
             return string.Format("The user name is {0}, and her new password is {1}", user, password);
         }
